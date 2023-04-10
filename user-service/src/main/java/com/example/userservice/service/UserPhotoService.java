ce44aed1c65c003d13a522bf53cd2738cece1798
nevels <neevels@mail.ru>
2023-04-10 10:38:23 +0300

    Update

--- user-service/src/main/java/com/example/userservice/service/UserPhotoService.java
+++ user-service/src/main/java/com/example/userservice/service/UserPhotoService.java
@@ -11,7 +11,7 @@ import java.io.File;
  */
 
 public interface UserPhotoService {
-    File getUserPhoto();
+    String getUserPhoto();
     String addUserPhoto(MultipartFile multipartFile);
     void updateUserPhoto(String path, MultipartFile multipartFile);
     String deleteUserPhoto();
